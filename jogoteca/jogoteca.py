6779c092c363742dda9ee91b3656b50f7d4fb7fa
Felipe <felipe.yajimabatista@gmail.com>
2022-03-08 21:18:14 -0300

    continuacao do curso

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session, flash
 
 ## helper render_template serve para renderizar paginas html
 ## helper request fornece como variavel global, todas informacoes da request, inclusive as informacoes do Formulario html
 
 app = Flask(__name__)
 
+## necessario ao usar session
+app.secret_key = 'caelum'
 class Jogo:
     def __init__(self, nome, categoria, console):
         self.nome = nome
@@ -23,6 +25,8 @@ def index():
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login?proxima=novo')
     return render_template('novo.html', titulo='Novo Jogo')
 
 @app.route('/criar', methods=['POST',])
@@ -32,8 +36,31 @@ def criar():
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return render_template('lista.html', titulo="Jogo", jogos=lista)
+    return redirect('/')
 
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'mestra' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(request.form['usuario'] + ' logou com sucesso')
+        proxima_pagina = request.form['proxima']
+        return redirect('/{}'.format(proxima_pagina))
+    else:
+        flash('Não logado, tente novamente')
+        return redirect('/login')
+
+
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Nenhum usuario logado')
+    return redirect('/')
 
 ## o debug ajuda a nao ficar tendo q restartar toda hora o webservice
 app.run(debug=True)
